a3519ae00e3b1a9ca58e73dcbd502a4d80a1104b
Samuel Luna <samuhluna@gmail.com>
2024-02-11 10:05:35 -0300

    Adicionado Package ornagizado por semana

---
+++ Java-anatomia-classes/src/edu/samuel/primeirasemana/variaveis.java
@@ -0,0 +1,5 @@
+package edu.samuel.primeirasemana;
+
+public class variaveis {
+
+}
---
+++ meuPrimeiroProjetoJava/src/Entities/Count.java
@@ -0,0 +1,15 @@
+package Entities;
+
+public class Count {
+    
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String setNome() {
+        return nome;
+    }
+}
