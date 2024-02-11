41c31fda6dddb1d43b4297d011eb38bb01faa818
Samuel Luna <samuhluna@gmail.com>
2024-02-11 10:39:59 -0300

    básico de variaveis e constantes

--- Java-anatomia-classes/src/edu/samuel/primeirasemana/variaveis.java
+++ Java-anatomia-classes/src/edu/samuel/primeirasemana/variaveis.java
@@ -3,5 +3,24 @@ package edu.samuel.primeirasemana;
 public class Variaveis {
     public static void main(String[] args) {
         
+        double salarioMedio = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
+        String nomeCompleto = "Samuel";
+        double valorSalvdo = 10.25;
+        
+        int numero = 1;
+
+        numero = 2;
+
+        // Constantes
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
