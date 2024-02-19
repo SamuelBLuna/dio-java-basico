76d68ee5ee1c68b4bddef013c6fd387012a658fc
Samuel Luna <samuhluna@gmail.com>
2024-02-18 23:01:43 -0300

    Get and Set

---
+++ Intellij/escola/src/escola/Aluno.java
@@ -0,0 +1,33 @@
+package escola;
+
+public class Aluno {
+
+    private String nome;
+    private int idade;
+
+    private  char sex;
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ Intellij/escola/src/escola/Escola.java
@@ -0,0 +1,16 @@
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+
+        Aluno felipe = new Aluno();
+
+        felipe.setNome("Felipe Torres");
+        felipe.setSex('M');
+        felipe.setIdade(8);
+
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+        //RESULTADO NO CONSOLE
+        //O aluno Felipe tem 8 anos
+    }
+}
